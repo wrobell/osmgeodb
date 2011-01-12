7d29ee6540e82d098bf4c6f528ab045dbb297cec
wrobell <wrobell@pld-linux.org>
2011-01-12 01:15:21 +0000

    - added examples of R scripts to draw border lines and water areas from   osm data uploaded to a database

---
+++ examples/borders.R
@@ -0,0 +1,21 @@
+require(RPostgreSQL)
+library(rgeos)
+
+q = sprintf("
+select 
+    id,
+    points::varchar,
+    st_astext(shape) as shape
+from osm_line ol
+where ((tags->'admin_level') = '%s')", 2)
+
+cn = dbConnect(PostgreSQL(), dbname='pl')
+d = dbGetQuery(cn, q)
+coords = readWKT(paste("GEOMETRYCOLLECTION(", paste(d$shape, collapse=","), ")", sep=""))
+data = SpatialLinesDataFrame(coords, d)
+
+pdf('borders.pdf', title='Borders')
+plot(data)
+dev.off()
+
+# vim: sw=4:et:ai
---
+++ examples/water.R
@@ -0,0 +1,21 @@
+require(RPostgreSQL)
+library(rgeos)
+
+q = sprintf("
+select 
+    id,
+    (tags->'name'),
+    st_astext(shape) as shape
+from osm_area
+where ((tags->'natural') = 'water')")
+
+cn = dbConnect(PostgreSQL(), dbname='pl')
+d = dbGetQuery(cn, q)
+coords = readWKT(paste("GEOMETRYCOLLECTION(", paste(d$shape, collapse=","), ")", sep=""))
+data = SpatialPolygonsDataFrame(coords, d)
+
+pdf('water.pdf', title='Water')
+plot(data)
+dev.off()
+
+# vim: sw=4:et:ai
